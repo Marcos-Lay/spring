f7ef5f4dd2fcdf851e948a67cfe6a2f01c06d237
8143 <16619975586@163.com>
2019-07-30 13:43:04 +0800

    创建spring-core,引入spring-context依赖

---
+++ spring_context/src/main/java/com/malei/demo/TestClassPathXmlApplicationContext.java
@@ -0,0 +1,11 @@
+package com.malei.demo;
+
+/**
+ * @program: spring
+ * @description: spring入口类
+ * @author: Marcos-Lay
+ * @create: 2019-07-30 11:35
+ **/
+public class TestClassPathXmlApplicationContext {
+
+}
